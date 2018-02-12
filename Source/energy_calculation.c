f783ffb930d5069ef2a2f0ff8ec836a1782f6d3c
xzcx <nuno360@gmail.com>
2018-02-12 19:49:48 +0000

    Fixed indentation and style in energy_calculation.c

--- Source/energy_calculation.c
+++ Source/energy_calculation.c
@@ -1,5 +1,5 @@
 /* Feynman  - A simple astronomy simulator written in the C programming language
- * Copyright (C) 2016-2017 Nuno Ferreira
+ * Copyright (C) 2016-2018 Nuno Ferreira
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,27 +21,32 @@
 
 void calculate_energies(Energy *e){
 
-double KE = 0;
-double PE = 0;
+    double KE = 0;
+    double PE = 0;
 
-register int n;	
-register int l;
+    register int n;	
+    register int l;
 	
-for (n=0; n<N_planets; n++){
-	KE = (KE + 0.5*planet[n].m*(pow(planet[n].vx,2) + pow(planet[n].vy,2) + pow(planet[n].vz,2))  );  //Calculate Kinetic Energy of the system of particles
-}                                                                           //K = 0.5*m*(vx^2+vy^2)
+    for (n = 0; n < N_planets; n++)
+    {
+        KE = (KE + 0.5*planet[n].m*(pow(planet[n].vx,2) + pow(planet[n].vy,2) + pow(planet[n].vz,2))  );  /* Calculate Kinetic Energy of the system of particles   K = 0.5*m*(vx^2+vy^2) */
 
 
-for (n=0; n<N_planets; n++){
-for (l=0; l<N_planets; l++){ 
-    if(n>l){
-        PE = PE-G*(planet[l].m)*(planet[n].m)/(r[n][l]); }  //Calculate potential energy of the system      P=-G*m1*m2/r ;
-    else{
-        PE = PE;}
-}
-}
+    for (n = 0; n < N_planets; n++)
+    {
+        for (l = 0; l < N_planets; l++)
+        { 
+            if(n>l)
+            {
+                PE = PE - G*(planet[l].m)*(planet[n].m) / (r[n][l]); /* Calculate potential energy of the system   P=-G*m1*m2/r */
+            }  
+            else
+            {
+                PE = PE;
+	    }
+        }
+    }
 
-e->KE = KE;
-e->PE = PE;
-
+    e->KE = KE;
+    e->PE = PE;
 }
